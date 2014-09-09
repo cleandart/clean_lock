dcfeb54802f9fdc89db5662e03bdb97da83bfca3
jurom <jurajmchc@gmail.com>
2014-09-09 14:16:16 +0200

    test added

---
+++ test/test_lock.dart
@@ -0,0 +1,30 @@
+import "package:unittest/unittest.dart";
+import 'dart:async';
+import 'package:clean_lock/lock_requestor.dart';
+
+main() {
+  run();
+}
+
+run() {
+
+  LockRequestor lockRequestor;
+
+  setUp(() {
+    return LockRequestor.connect("127.0.0.1", 27002)
+        .then((LockRequestor lockR) => lockRequestor = lockR);
+  });
+
+  test("withLock should throw if callback is not waiting for futures", () {
+        var callback = () {
+          Future lateFuture = new Future.delayed(new Duration(milliseconds:300),
+              () => lockRequestor.withLock("random lock",() {}));
+          return new Future.value(null);
+        };
+        bool caughtError = false;
+        runZoned(() {
+            lockRequestor.withLock("random lock",callback);
+        }, onError: (e) => caughtError = true);
+        return new Future.delayed(new Duration(milliseconds:800), () => expect(caughtError,isTrue));
+      });
+}
