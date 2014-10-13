4b299c19cce68563115ec008ae61d2b2dc928423
Martin Pitoňák <piton4k@gmail.com>
2014-10-13 14:33:54 +0200

    fixed example lock types

--- example/lock_requestor.dart
+++ example/lock_requestor.dart
@@ -7,7 +7,7 @@ Random _random = new Random();
 void acquireLock(LockRequestor lockRequestor, String id, Duration duration) {
   var i = _random.nextInt(4);
   var authorId = i != 0 ? "example-author$i" : null;
-  lockRequestor.withLock("example-lock$id", () => new Future.delayed(duration), author: authorId);
+  lockRequestor.withLock("example-lock-$id", () => new Future.delayed(duration), author: authorId);
 }
 
 Duration seconds(int seconds) {
@@ -21,18 +21,18 @@ Duration minutes(int minutes) {
 void main() {
   LockRequestor.connect("127.0.0.1", 27002)
     .then((LockRequestor lockRequestor) {
-      acquireLock(lockRequestor, "example-lock1", seconds(5));
-      acquireLock(lockRequestor, "example-lock1", minutes(5));
-      acquireLock(lockRequestor, "example-lock1", minutes(5));
+      acquireLock(lockRequestor, "1", seconds(5));
+      acquireLock(lockRequestor, "1", minutes(5));
+      acquireLock(lockRequestor, "1", minutes(5));
 
-      acquireLock(lockRequestor, "example-lock2", minutes(5));
-      acquireLock(lockRequestor, "example-lock2", seconds(0));
-      acquireLock(lockRequestor, "example-lock2", minutes(5));
+      acquireLock(lockRequestor, "2", minutes(5));
+      acquireLock(lockRequestor, "2", seconds(0));
+      acquireLock(lockRequestor, "2", minutes(5));
 
-      acquireLock(lockRequestor, "example-lock3", seconds(0));
+      acquireLock(lockRequestor, "3", seconds(0));
 
-      acquireLock(lockRequestor, "example-lock4", minutes(5));
-      acquireLock(lockRequestor, "example-lock4", minutes(5));
-      acquireLock(lockRequestor, "example-lock4", minutes(5));
+      acquireLock(lockRequestor, "4", minutes(5));
+      acquireLock(lockRequestor, "4", minutes(5));
+      acquireLock(lockRequestor, "4", minutes(5));
     });
 }
